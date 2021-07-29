e14cc93b4654192e7db47e323a041494925cbed6
wasya1212 <wasya1212cool@gmail.com>
2021-07-29 11:25:46 +0300

    fixed to nan

--- index.test.js
+++ index.test.js
@@ -16,16 +16,16 @@ describe('Search for pallindromes and sqr pallindromes', () => {
   });
 
   test('Should return null', () => {
-    expect(checkPalindrome()).toBeNull();
-    expect(checkPalindrome(null)).toBeNull();
-    expect(checkPalindrome(NaN)).toBeNull();
-    expect(checkPalindrome({})).toBeNull();
-    expect(checkPalindrome([])).toBeNull();
-    expect(checkPalindrome('word')).toBeNull();
-    expect(checkPalindrome('100word')).toBeNull();
-    expect(checkPalindrome(jest.fn())).toBeNull();
-    expect(checkPalindrome(false)).toBeNull();
-    expect(checkPalindrome(true)).toBeNull();
+    expect(checkPalindrome()).toBeNaN();
+    expect(checkPalindrome(null)).toBeNaN();
+    expect(checkPalindrome(NaN)).toBeNaN();
+    expect(checkPalindrome({})).toBeNaN();
+    expect(checkPalindrome([])).toBeNaN();
+    expect(checkPalindrome('word')).toBeNaN();
+    expect(checkPalindrome('100word')).toBeNaN();
+    expect(checkPalindrome(jest.fn())).toBeNaN();
+    expect(checkPalindrome(false)).toBeNaN();
+    expect(checkPalindrome(true)).toBeNaN();
   });
 
   test('Should to be an array as result', () => {
@@ -68,16 +68,16 @@ describe('Search for the sum and count of numbers that are divisible by 5 and no
   });
 
   test('Should return null', () => {
-    expect(getQuantity()).toBeNull();
-    expect(getQuantity(null)).toBeNull();
-    expect(getQuantity(NaN)).toBeNull();
-    expect(getQuantity({})).toBeNull();
-    expect(getQuantity([])).toBeNull();
-    expect(getQuantity('word')).toBeNull();
-    expect(getQuantity('100word')).toBeNull();
-    expect(getQuantity(jest.fn())).toBeNull();
-    expect(getQuantity(false)).toBeNull();
-    expect(getQuantity(true)).toBeNull();
+    expect(getQuantity()).toBeNaN();
+    expect(getQuantity(null)).toBeNaN();
+    expect(getQuantity(NaN)).toBeNaN();
+    expect(getQuantity({})).toBeNaN();
+    expect(getQuantity([])).toBeNaN();
+    expect(getQuantity('word')).toBeNaN();
+    expect(getQuantity('100word')).toBeNaN();
+    expect(getQuantity(jest.fn())).toBeNaN();
+    expect(getQuantity(false)).toBeNaN();
+    expect(getQuantity(true)).toBeNaN();
   });
 
   test('Should throw "natural number" error', () => {
@@ -136,16 +136,16 @@ describe('Search for digit crossing of two arrays', () => {
   });
 
   test('Should return null', () => {
-    expect(getCrossing()).toBeNull();
-    expect(getCrossing(null)).toBeNull();
-    expect(getCrossing(NaN)).toBeNull();
-    expect(getCrossing({})).toBeNull();
-    expect(getCrossing([])).toBeNull();
-    expect(getCrossing('word')).toBeNull();
-    expect(getCrossing('100word')).toBeNull();
-    expect(getCrossing(jest.fn())).toBeNull();
-    expect(getCrossing(false)).toBeNull();
-    expect(getCrossing(true)).toBeNull();
+    expect(getCrossing()).toBeNaN();
+    expect(getCrossing(null)).toBeNaN();
+    expect(getCrossing(NaN)).toBeNaN();
+    expect(getCrossing({})).toBeNaN();
+    expect(getCrossing([])).toBeNaN();
+    expect(getCrossing('word')).toBeNaN();
+    expect(getCrossing('100word')).toBeNaN();
+    expect(getCrossing(jest.fn())).toBeNaN();
+    expect(getCrossing(false)).toBeNaN();
+    expect(getCrossing(true)).toBeNaN();
   });
 
   test('Should throw "natural number" error', () => {
